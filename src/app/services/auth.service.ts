492cb351e321901457ff7d30bbdce6e6b82273c2
IT046004 <antonino.samperi@kikocosmetics.com>
2023-05-05 12:43:21 +0200

    DEVS

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,30 +1,68 @@
-import { Injectable } from '@angular/core';
-import firebase from 'firebase/app';
-
+import {Injectable} from "@angular/core";
+import {
+    GoogleAuthProvider,
+    getAuth,
+    signInWithPopup,
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
+    sendPasswordResetEmail,
+    signOut,
+    Auth,
+    UserCredential
+} from "firebase/auth";
+import {
+    getFirestore,
+    query,
+    getDocs,
+    collection,
+    where,
+    addDoc,
+    Firestore
+} from "firebase/firestore";
+import {FirebaseApp, FirebaseOptions, initializeApp} from "firebase/app";
 
 @Injectable()
 export class AuthService {
 
-  private loggedInStatus = JSON.parse(
-    localStorage.getItem('loggedIn') || 'false'
-  );
+    get config(): FirebaseOptions {
+        return {
+            apiKey: "AIzaSyBwkpfDQdLYLH9J7jZNSSJPOHiCncGnjW8",
+            authDomain: "kiko-login-tests.firebaseapp.com",
+            projectId: "kiko-login-tests",
+            storageBucket: "kiko-login-tests.appspot.com",
+            messagingSenderId: "801479242798",
+            appId: "1:801479242798:web:d445135028433c0c1e5bbf"
+        };
+    }
 
-  setLoginStatus(value: boolean) {
-    this.loggedInStatus = value;
-    localStorage.setItem('loggedIn', 'true');
-  }
+    get loginStatus() {
+        return JSON.parse(
+            localStorage.getItem("loggedIn") || this._loggedInStatus.toString()
+        );
+    }
 
-  get loginStatus() {
-    return JSON.parse(
-      localStorage.getItem('loggedIn') || this.loggedInStatus.toString()
+    private _app: FirebaseApp = initializeApp(this.config);
+    private _auth: Auth;
+
+    private _loggedInStatus = JSON.parse(
+        localStorage.getItem("loggedIn") || "false"
     );
-  }
 
-  signUpUser(email: string, password: string) {
-    return firebase.auth().createUserWithEmailAndPassword(email, password);
-  }
+    constructor() {
+        this._auth = getAuth(this._app);
+    }
 
-  signInUser(email: string, password: string) {
-    return auth().signInWithEmailAndPassword(email, password);
-  }
+    setLoginStatus(value: boolean) {
+        this._loggedInStatus = value;
+        localStorage.setItem("loggedIn", "true");
+    }
+
+    signUpUser(email: string, password: string) {
+        return createUserWithEmailAndPassword(this._auth, email, password);
+    }
+
+    signInUser(email: string, password: string) {
+        return signInWithEmailAndPassword(this._auth, email, password);
+    }
+
 }
